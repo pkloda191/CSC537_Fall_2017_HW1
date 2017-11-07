5155c1111fbfd9e9a1891b6d56a813e45923038a
pkloda191 <pkloda191@gmail.com>
2017-11-06 23:15:40 -0600

    added comment

--- app/src/main/java/com/example/awesomefat/csc537_fall2017_hw1/MainActivity.java
+++ app/src/main/java/com/example/awesomefat/csc537_fall2017_hw1/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //Peter Kloda
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.activity_main);
         this.svLayout = (ViewGroup)this.findViewById(R.id.svLayout);
